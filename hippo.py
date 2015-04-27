ea123114501ef896578bd4b8d8b9eb728d0e8420
Cristina Mani <misha.mani@yahoo.com>
2015-04-27 13:39:58 +0200

    added health variable and 2 more animals

--- hippo.py
+++ hippo.py
@@ -3,6 +3,7 @@
 class Animal(object):
     """Makes cute animals."""
     is_alive = True
+    health = "good"    
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -10,5 +11,10 @@ class Animal(object):
         print self.name
         print self.age
         
+sloth = Animal("Pixie", 1)
+ocelot = Animal("Tooth_Fairy", 3)    
 hippo = Animal("George", 27)
+print hippo.health
+print sloth.health
+print ocelot.health
 print hippo.description()
