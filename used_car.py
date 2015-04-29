11481d68387307029438108724596268c8657dba
Cristina Mani <misha.mani@yahoo.com>
2015-04-29 14:16:29 +0200

    car new, used, like new

--- used_car.py
+++ used_car.py
@@ -19,3 +19,21 @@ print my_car.condition
 
 my_car.drive_car()
 print my_car.condition
+
+class ElectricCar(Car):
+    def __init__(self, model, color, mpg, battery_type):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+        self.battery_type = battery_type
+    
+    def drive_car(self):
+        self.condition = "like new"
+        
+print my_car.condition
+my_car = ElectricCar("DeLorean", "silver", 88, "molten salt")
+
+my_car.drive_car()
+print my_car.condition
+
+my_car = ElectricCar("DeLorean", "silver", 88, "molten salt")
